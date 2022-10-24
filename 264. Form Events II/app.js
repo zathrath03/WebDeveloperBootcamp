112f5a5459174c26cf040bf2975bcc818127687d
John Dugger <zathrath03@gmail.com>
2022-10-24 13:06:03 -0700

    (Form Events II) Switched to using elements within the form that we grabbed using querySelector instead of using querySelector to grab each element individually

--- 264. Form Events II/app.js
+++ 264. Form Events II/app.js
@@ -1,11 +1,12 @@
 const form = document.querySelector("#tweets");
-const tweet = document.querySelector("#tweet");
-const user = document.querySelector("#user");
-const list = document.querySelector("#tweet_list")
+const list = document.querySelector("#tweet_list");
+
 form.addEventListener("submit", function (e) {
     e.preventDefault();
+    const tweet = form.elements.tweet.value;
+    const user = form.elements.username.value;
     newLI = document.createElement("LI");
-    newLI.innerText = `${user.value} says ${tweet.value}`
+    newLI.innerText = `${user} says ${tweet}`
     list.append(newLI)
     tweet.value = "";
     user.value = "";
