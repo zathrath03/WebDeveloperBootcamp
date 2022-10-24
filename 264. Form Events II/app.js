3ec2811f846a33ca73027c8be5bde3d1ae04207c
John Dugger <zathrath03@gmail.com>
2022-10-24 13:20:01 -0700

    (Form Events II) Using innerHTML and bold tags

--- 264. Form Events II/app.js
+++ 264. Form Events II/app.js
@@ -3,11 +3,11 @@ const list = document.querySelector("#tweet_list");
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    const tweet = form.elements.tweet.value;
-    const user = form.elements.username.value;
+    const tweet = form.elements.tweet;
+    const user = form.elements.username;
     newLI = document.createElement("LI");
-    newLI.innerText = `${user} says ${tweet}`
-    list.append(newLI)
+    newLI.innerHTML = `<b>${user.value}</b> says ${tweet.value}`;
+    list.append(newLI);
     tweet.value = "";
     user.value = "";
 });
