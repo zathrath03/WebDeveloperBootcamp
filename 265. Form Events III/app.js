6ac648427943d29ecdc641d3e930382fbd198881
John Dugger <zathrath03@gmail.com>
2022-10-24 16:44:58 -0700

    (Form Events III) Full implementation

--- 265. Form Events III/app.js
+++ 265. Form Events III/app.js
@@ -1,2 +1,14 @@
 // Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
 const form = document.querySelector('form');
+const list = document.querySelector('#list');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const product = form.elements.product;
+    const quantity = form.elements.qty;
+    const newLI = document.createElement("LI");
+    newLI.innerText = `${quantity.value} ${product.value}`;
+    list.append(newLI);
+    product.value = "";
+    quantity.value = "";
+});
