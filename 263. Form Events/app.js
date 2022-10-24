4feca4e0e30a39919db6a3a0eea12e9705c45686
John Dugger <zathrath03@gmail.com>
2022-10-24 11:16:35 -0700

    (263 Form Events) Exercise to create a list from strings input into a text field

--- 263. Form Events/app.js
+++ 263. Form Events/app.js
@@ -1,5 +1,11 @@
 const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName");
+const list = document.querySelector("#cats");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log("SUBMITTED THE FORM")
+    const catName = input.value;
+    const newLI = document.createElement("LI");
+    newLI.innerText = catName
+    list.append(newLI)
+    input.value = ""
 })
