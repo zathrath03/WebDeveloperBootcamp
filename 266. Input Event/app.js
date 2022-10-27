bc665be188b8c039573c2dd1fdf98ced26607ea6
John Dugger <zathrath03@gmail.com>
2022-10-26 20:01:13 -0700

    (266. Input Event) Given HTML & problem statement

---
+++ 266. Input Event/app.js
@@ -0,0 +1,9 @@
+// Input Event Practice
+// It's time to practice working with the input event!  In the index.html file, you'll find an <h1> and an <input type="text"> element.  Please do not change anything in index.html!  In app.js, write code that meets these requirements:
+
+// The h1 should start with the text "Enter Your Username" (I've done that for you, already in the markup)
+
+// Whenever an input event is fired on the <input> element, update the <h1> so that it displays "Welcome, " plus the current value from the text input.  Take a look at the gif below to see how it should work.
+
+// If the <input> goes back to being empty, update the <h1> so that it once again says "Enter Your Username"
+
