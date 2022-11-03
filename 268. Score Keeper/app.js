a9861354a38bd00a6a41b21a9aba93e6484f723d
John Dugger <zathrath03@gmail.com>
2022-11-02 18:44:38 -0700

    (268) app.js - move reset() to its own function - allows use with changing the winning_score

--- 268. Score Keeper/app.js
+++ 268. Score Keeper/app.js
@@ -33,11 +33,13 @@ function check_winner(player_score) {
 
 }
 
-reset_button.addEventListener('click', function () {
+reset_button.addEventListener('click', reset);
+
+function reset() {
     player_1_score.innerText = "0";
     player_2_score.innerText = "0";
     player_1_score.style.color = "black";
     player_2_score.style.color = "black";
     player_1_button.disabled = false;
     player_2_button.disabled = false;
-})
+}
