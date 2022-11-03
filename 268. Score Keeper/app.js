95ff1ddc10ded6ff94df9af6f880c4f4591543ad
John Dugger <zathrath03@gmail.com>
2022-11-02 18:26:14 -0700

    (268) app.js - Add all query selectors

--- 268. Score Keeper/app.js
+++ 268. Score Keeper/app.js
@@ -6,23 +6,3 @@ player_1_button = document.querySelector("#increment_player_1");
 player_2_button = document.querySelector("#increment_player_2");
 reset_button = document.querySelector("#reset");
 
-player_1_button.addEventListener('click', function () {
-    current_score = parseInt(player_1_score.innerText);
-    player_1_score.innerText = current_score + 1;
-    check_winner(player_1_score);
-});
-
-function check_winner(player_score) {
-    if (player_score.innerText === winning_score.value) {
-        if (player_score === player_1_score) {
-            player_1_score.style.color = "green";
-            player_2_score.style.color = "red";
-        } else {
-            player_1_score.style.color = "red";
-            player_2_score.style.color = "green";
-        }
-        player_1_button.disabled = true;
-        player_2_button.disabled = true;
-    }
-
-}
