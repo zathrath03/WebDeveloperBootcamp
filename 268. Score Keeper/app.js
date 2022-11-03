33f0f7116bb440080f24ba6c15a74d8b6250805b
John Dugger <zathrath03@gmail.com>
2022-11-02 18:27:31 -0700

    (268) app.js - increment button event listeners

--- 268. Score Keeper/app.js
+++ 268. Score Keeper/app.js
@@ -6,3 +6,14 @@ player_1_button = document.querySelector("#increment_player_1");
 player_2_button = document.querySelector("#increment_player_2");
 reset_button = document.querySelector("#reset");
 
+player_1_button.addEventListener('click', function () {
+    current_score = parseInt(player_1_score.innerText);
+    player_1_score.innerText = current_score + 1;
+    check_winner(player_1_score);
+});
+
+player_2_button.addEventListener('click', function () {
+    current_score = parseInt(player_2_score.innerText);
+    player_2_score.innerText = current_score + 1;
+    check_winner(player_2_score);
+});
