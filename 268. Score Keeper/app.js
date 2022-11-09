40210bac1e96ee7ae249c8993994cb1ca1b01788
John Dugger <zathrath03@gmail.com>
2022-11-08 16:40:31 -0800

    (268) app.js - refactor update_score_colors into a separate function

--- 268. Score Keeper/app.js
+++ 268. Score Keeper/app.js
@@ -10,9 +10,8 @@ player_1_button.addEventListener('click', function () {
     current_score = parseInt(player_1_score.innerText);
     player_1_score.innerText = current_score + 1;
     if (is_winner(player_1_score)) {
-        disable_player_buttons()
-        player_1_score.style.color = "green";
-        player_2_score.style.color = "red";
+        disable_player_buttons();
+        update_score_colors(player_1_score, player_2_score);
     }
 });
 
@@ -20,17 +19,21 @@ player_2_button.addEventListener('click', function () {
     current_score = parseInt(player_2_score.innerText);
     player_2_score.innerText = current_score + 1;
     if (is_winner(player_2_score)) {
-        disable_player_buttons()
-        player_1_score.style.color = "red";
-        player_2_score.style.color = "green";
+        disable_player_buttons();
+        update_score_colors(player_2_score, player_1_score);
     }
 });
 
 reset_button.addEventListener('click', reset);
 winning_score.addEventListener('change', reset);
 
+function update_score_colors(player, opponent) {
+    player.classList.add('winner');
+    opponent.classList.add('loser');
+}
+
 function is_winner(player_score) {
-    return player_score.innerText === winning_score.value
+    return player_score.innerText === winning_score.value;
 }
 
 function disable_player_buttons() {
@@ -41,8 +44,8 @@ function disable_player_buttons() {
 function reset() {
     player_1_score.innerText = "0";
     player_2_score.innerText = "0";
-    player_1_score.style.color = "black";
-    player_2_score.style.color = "black";
+    player_1_score.classList.remove('winner', 'loser');
+    player_2_score.classList.remove('winner', 'loser');
     player_1_button.disabled = false;
     player_2_button.disabled = false;
 }
