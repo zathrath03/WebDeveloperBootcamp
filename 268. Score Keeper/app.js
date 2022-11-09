c48439017e109cebc1a4c512acbb5b5faa8c6ad6
John Dugger <zathrath03@gmail.com>
2022-11-08 16:21:50 -0800

    (268) app.js cleanup the check_winner function - split is_winner to its own function - split disable_player_buttons to its own function - move color changing to the event listener

--- 268. Score Keeper/app.js
+++ 268. Score Keeper/app.js
@@ -9,32 +9,34 @@ const reset_button = document.querySelector("#reset");
 player_1_button.addEventListener('click', function () {
     current_score = parseInt(player_1_score.innerText);
     player_1_score.innerText = current_score + 1;
-    check_winner(player_1_score);
+    if (is_winner(player_1_score)) {
+        disable_player_buttons()
+        player_1_score.style.color = "green";
+        player_2_score.style.color = "red";
+    }
 });
 
 player_2_button.addEventListener('click', function () {
     current_score = parseInt(player_2_score.innerText);
     player_2_score.innerText = current_score + 1;
-    check_winner(player_2_score);
-});
-
-function check_winner(player_score) {
-    if (player_score.innerText === winning_score.value) {
-        if (player_score === player_1_score) {
-            player_1_score.style.color = "green";
-            player_2_score.style.color = "red";
-        } else {
-            player_1_score.style.color = "red";
-            player_2_score.style.color = "green";
-        }
-        player_1_button.disabled = true;
-        player_2_button.disabled = true;
+    if (is_winner(player_2_score)) {
+        disable_player_buttons()
+        player_1_score.style.color = "red";
+        player_2_score.style.color = "green";
     }
-
-}
+});
 
 reset_button.addEventListener('click', reset);
 winning_score.addEventListener('change', reset);
+
+function is_winner(player_score) {
+    return player_score.innerText === winning_score.value
+}
+
+function disable_player_buttons() {
+    player_1_button.disabled = true;
+    player_2_button.disabled = true;
+}
 
 function reset() {
     player_1_score.innerText = "0";
