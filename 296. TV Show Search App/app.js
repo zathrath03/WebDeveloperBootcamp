dfd7ad1d28979e78b7650feb1f7f9b0ceb199d54
John Dugger <zathrath03@gmail.com>
2022-11-19 15:12:03 -0800

    (296) Add all returned images to the page

--- 296. TV Show Search App/app.js
+++ 296. TV Show Search App/app.js
@@ -3,5 +3,15 @@ form.addEventListener('submit', async function (e) {
     e.preventDefault()
     const input = form.elements.query.value
     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${input}`)
-    console.log(res)
+    addImages(res.data)
 })
+
+function addImages(shows) {
+    for (let show of shows) {
+        if (show.show.image) {
+            const img = document.createElement("IMG")
+            img.src = show.show.image.medium
+            document.body.append(img)
+        }
+    }
+}
