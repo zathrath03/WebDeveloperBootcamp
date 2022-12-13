91e460e6eebf0c54e8f05f08a84db90e2946752d
John Dugger <zathrath03@gmail.com>
2022-12-13 15:46:22 -0800

    (333) Implementation

--- 333. Language Guesser Challenge/index.js
+++ 333. Language Guesser Challenge/index.js
@@ -4,3 +4,12 @@ const require = createRequire(import.meta.url);
 
 import { franc } from 'franc'
 const langs = require("langs")
+
+const inputString = process.argv[2]
+const languageCode = franc(inputString)
+if (languageCode === "und") {
+    console.log("Could not match a language. Please try again with a larger sample")
+} else {
+    const languageName = langs.where("3", languageCode).name
+    console.log(languageName)
+}
