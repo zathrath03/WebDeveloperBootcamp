9c671cf6d8d483242e442e87558af6fff823f7ec
John Dugger <zathrath03@gmail.com>
2022-12-13 15:37:20 -0800

    (333) Initial package and requires

---
+++ 333. Language Guesser Challenge/index.js
@@ -0,0 +1,6 @@
+// These lines make "require" available
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+import { franc } from 'franc'
+const langs = require("langs")
